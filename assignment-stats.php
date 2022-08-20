185aa70e24679d3796136b5f8b6fd74ad1c38950
Rodrigo Rocha <rodrigorgs@gmail.com>
2022-08-20 15:03:07 -0300

    Add assignment-stats

---
+++ assignment-stats.php
@@ -0,0 +1,57 @@
+<?php
+  require_once __DIR__ . '/includes.php';
+
+  header('Content-Type: text/csv');
+  header('Content-Disposition: inline');
+
+  $assignment_url = $_GET["url"];
+  $submission_type = $_GET["submission_type"] ?? null;
+
+  // Connect to the database
+  $conn = mysqli_connect($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_DATABASE, $DB_PORT);
+  if (!$conn) {
+    http_response_code(500);
+    die('Erro de banco de dados: ' . mysqli_connect_error());
+  }
+  mysqli_query($conn, "SET NAMES 'utf8'");
+
+  $query_string = "
+  SELECT username, question_index
+  FROM `sub_submissions`
+  WHERE `assignment_url` = ?";
+  if ($submission_type) {
+    $query_string .= " AND `submission_type` = ?";
+  }
+  $query_string .= "
+  GROUP BY username, question_index
+  ORDER BY username, question_index
+  ";
+  $sql = $conn->prepare($query_string);
+  if ($submission_type) {
+    $sql->bind_param("ss", $assignment_url, $submission_type);
+  } else {
+    $sql->bind_param("s", $assignment_url);
+  }
+  $sql->execute();
+  $result = $sql->get_result();
+
+  $users = array();
+  $max_question_index = 0;
+  while($row = $result->fetch_assoc()) {
+    $question_index = $row["question_index"];
+    $users[$row["username"]][intval($question_index)] = 1;
+    if ($question_index > $max_question_index) {
+      $max_question_index = $question_index;
+    }
+  }
+
+  echo "username\ttotal\t" . implode("\t", range(0, $max_question_index)) . "\n";
+  $question_range = range(0, $max_question_index);
+  foreach ($users as $username => $questions) {
+    echo $username . "\t" . count($questions) . "\t";
+
+    $q = array_map(function($idx) use ($questions, $max_question_index) { return array_key_exists($idx, $questions) ? 1 : 0; }, $question_range);
+    echo implode("\t", $q) . "\n";
+  }
+
+?>
