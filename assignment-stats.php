89ca7eda7964eb9b120a33ccbb55f265b960ce8e
Rodrigo Rocha <rodrigorgs@gmail.com>
2022-08-21 23:07:25 -0300

    Improve assignment stats

--- assignment-stats.php
+++ assignment-stats.php
@@ -1,9 +1,6 @@
 <?php
   require_once __DIR__ . '/includes.php';
 
-  header('Content-Type: text/csv');
-  header('Content-Disposition: inline');
-
   $assignment_url = $_GET["url"];
   $submission_type = $_GET["submission_type"] ?? "batch";
 
@@ -37,13 +34,62 @@
     }
   }
 
-  echo "username\ttotal\t" . implode("\t", range(0, $max_question_index)) . "\n";
+  $table = [];
+  // echo "username\ttotal\t" . implode("\t", range(0, $max_question_index)) . "\n";
+  $table[] = array_merge(['username', 'total'], range(0, $max_question_index));
   $question_range = range(0, $max_question_index);
   foreach ($users as $username => $questions) {
-    echo $username . "\t" . count($questions) . "\t";
+    $row = [$username, count($questions)];
+    // $table[] = [$username, count($questions)];
+    // echo $username . "\t" . count($questions) . "\t";
 
     $q = array_map(function($idx) use ($questions, $max_question_index) { return array_key_exists($idx, $questions) ? 1 : 0; }, $question_range);
-    echo implode("\t", $q) . "\n";
+    $row = array_merge($row, $q);
+    // echo implode("\t", $q) . "\n";
+    $table[] = $row;
+  }
+
+  $mime = getBestSupportedMimeType(array('text/csv', 'text/html'));
+  error_log("mime: $mime");
+  if ($mime == 'text/html') {
+    header('Content-Type: text/html');
+    echo "$assignment_url<br>";
+    echo "submission_type: $submission_type<br>";
+    echo "token: " . getBearerToken() . "<br><br>";
+    echo "<table border=\"1\">";
+    echo "<th>" . implode("</th><th>", array_shift($table)) . "</th>";
+    foreach ($table as $row) {
+      echo "<tr>";
+      foreach ($row as $i => $cell) {
+        $query = http_build_query(array(
+          "url" => $assignment_url,
+          "username" => $row[0],
+          "question_index" => $i - 2,
+          "submission_type" => $submission_type,
+          "token" => getBearerToken()
+        ));
+        error_log("Bearer: " . getBearerToken());
+        if ($i <= 1) {
+          echo "<td>$cell</td>";
+        } else {
+          if ($cell == "1") {
+            echo "<td><a href=\"get-answer.php?$query\">X</a></td>";
+          } else {
+            echo "<td>&nbsp;</td>";
+          }
+        }
+      }
+    
+      echo "</tr>";
+    }
+    echo "</table>";
+  } else {
+    header('Content-Type: text/csv');
+    header('Content-Disposition: inline');
+
+    foreach ($table as $row) {
+      echo implode("\t", $row) . "\n";
+    }
   }
 
 ?>
--- includes.php
+++ includes.php
@@ -61,7 +61,7 @@
             return $matches[1];
         }
     }
-    return null;
+    return @$_GET['token'] ?? @$_POST['token'];
   }
 
   function getUsernameFromToken() {
@@ -103,6 +103,42 @@
     
         exit(0);
     }
+  }
+
+  // https://stackoverflow.com/questions/1049401/how-to-select-content-type-from-http-accept-header-in-php
+  function getBestSupportedMimeType($mimeTypes = null) {
+    // Values will be stored in this array
+    $AcceptTypes = Array ();
+
+    // Accept header is case insensitive, and whitespace isn’t important
+    $accept = strtolower(str_replace(' ', '', $_SERVER['HTTP_ACCEPT']));
+    // divide it into parts in the place of a ","
+    $accept = explode(',', $accept);
+    foreach ($accept as $a) {
+        // the default quality is 1.
+        $q = 1;
+        // check if there is a different quality
+        if (strpos($a, ';q=')) {
+            // divide "mime/type;q=X" into two parts: "mime/type" i "X"
+            list($a, $q) = explode(';q=', $a);
+        }
+        // mime-type $a is accepted with the quality $q
+        // WARNING: $q == 0 means, that mime-type isn’t supported!
+        $AcceptTypes[$a] = $q;
+    }
+    arsort($AcceptTypes);
+
+    // if no parameter was passed, just return parsed data
+    if (!$mimeTypes) return $AcceptTypes;
+
+    $mimeTypes = array_map('strtolower', (array)$mimeTypes);
+
+    // let’s check our supported types:
+    foreach ($AcceptTypes as $mime => $q) {
+       if ($q && in_array($mime, $mimeTypes)) return $mime;
+    }
+    // no mime-type found
+    return null;
   }
 
   function getCurrentQuestionIndex($conn, $assignment_url) {
